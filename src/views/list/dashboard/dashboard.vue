5d14180184eaf782882f3e346462548b5ee2ce2a
tangyin.it@ciic.com.cn <tangyin.it@ciic.com.cn>
2020-06-23 20:10:04 +0800

    修改vue中报错The template root requires exactly one element

--- src/views/list/dashboard/dashboard.vue
+++ src/views/list/dashboard/dashboard.vue
@@ -1,6 +1,7 @@
 <template>
-  <h1> this is dashboard </h1>
-  <div>
+  
+  <div style="width:100px;height:100px">
+      <h1> this is dashboard </h1>
       <p>这是dashboard </p>
   </div>
 </template>
