6d84f3ca240810b441c55230b0f58e121b72dea9
tangyin.it@ciic.com.cn <tangyin.it@ciic.com.cn>
2020-06-24 09:07:00 +0800

    修該了首頁頁面

--- src/views/list/dashboard/dashboard.vue
+++ src/views/list/dashboard/dashboard.vue
@@ -1,7 +1,52 @@
 <template>
-  
-  <div style="width:100px;height:100px">
-      <h1> this is dashboard </h1>
-      <p>这是dashboard </p>
+<div>
+  <div>
+    <h1>首頁table</h1>
+  </div>
+  <el-table
+    :data="tableData"
+    stripe
+    style="width: 100%">
+    <el-table-column
+      prop="date"
+      label="日期"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="name"
+      label="姓名"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="address"
+      label="地址">
+    </el-table-column>
+  </el-table>
   </div>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        tableData: [{
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }]
+      }
+    }
+  }
+</script>
